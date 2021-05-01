5341c2d7e815a2e871e5923be59aee20ed4f6160
fmolliet <fmolliet@hotmail.com>
2021-04-30 22:49:13 -0300

    Adicionando conteudo da aula 1 e 2 do alura, realizando chamada a outro microserviço pelo eureka discovery

--- src/main/java/br/com/alura/microservico/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservico/loja/service/CompraService.java
@@ -1,22 +1,29 @@
 package br.com.alura.microservico.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.alura.microservico.loja.controller.dto.CompraDTO;
 import br.com.alura.microservico.loja.controller.dto.InfoFornecedorDTO;
 
+@Service
 public class CompraService {
+	
+	@Autowired
+	private RestTemplate client;
 
     public void realizaCompra(CompraDTO compra) {
 
-        // É um cliente de request HTTP como axios
-        RestTemplate client = new RestTemplate();
+        // É um cliente de request HTTP como axios 
+    	// Passamos a logica de realizar o template para o Bean na classe main
+        //RestTemplate client = new RestTemplate();
         // o Client.exchange é onde passamos os parametros do que será executado, metodo
         // e como ele vai retornar
         ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-                "http://localhost:8081/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
+                "http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
                 InfoFornecedorDTO.class);
         System.out.println(exchange.getBody().getEndereco());
     }
